56e3b24d2b1d1621dd1f7620e2b6a4f5ff9914ef
Ajin-Roch <ajin.antony@finos.in>
2023-12-19 17:33:34 +0530

    add claims

---
+++ src/main/java/com/SpringSecuirty/JWT/config/JwtService.java
@@ -0,0 +1,12 @@
+package com.SpringSecuirty.JWT.config;
+
+
+//This is for the Jwt service section available 
+public class JwtService {
+
+	public  String extractUserName(String jwt) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
